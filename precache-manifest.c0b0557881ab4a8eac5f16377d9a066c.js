d41163faac419e4c57a4296066840dc3faff178d
marioarturo2000 <marioarturo2000@yahoo.com>
2022-09-03 11:56:19 -0500

    Add files via upload

---
+++ precache-manifest.c0b0557881ab4a8eac5f16377d9a066c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e603c365d269fa72883ae372283843",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b628a823ab087091151",
+    "url": "/static/css/2.7c485250.chunk.css"
+  },
+  {
+    "revision": "6b628a823ab087091151",
+    "url": "/static/js/2.3fea38cc.chunk.js"
+  },
+  {
+    "revision": "2b7709c301236828d8211d5a0aa6ea56",
+    "url": "/static/js/2.3fea38cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "607633797e307b259e99",
+    "url": "/static/js/main.a9bf67d1.chunk.js"
+  },
+  {
+    "revision": "3b1888f85a990ba2f335",
+    "url": "/static/js/runtime-main.d0973c79.js"
+  }
+]);
