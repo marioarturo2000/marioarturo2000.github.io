294ac254009f33b976af0e7d51df30971599ee8e
marioarturo2000 <marioarturo2000@yahoo.com>
2022-09-03 18:29:47 -0500

    Add files via upload

---
+++ precache-manifest.c1ea4d62c1b37982cf27ddcaf9b5d8c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b510b5e5324d8f45f22a43624d695cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "246b88105aa1dc849e40",
+    "url": "/static/css/2.7c485250.chunk.css"
+  },
+  {
+    "revision": "246b88105aa1dc849e40",
+    "url": "/static/js/2.476e978b.chunk.js"
+  },
+  {
+    "revision": "2b7709c301236828d8211d5a0aa6ea56",
+    "url": "/static/js/2.476e978b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "428cf68c1a5f93fba92b",
+    "url": "/static/js/main.29efd875.chunk.js"
+  },
+  {
+    "revision": "3b1888f85a990ba2f335",
+    "url": "/static/js/runtime-main.d0973c79.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.86e3c099298100a5910f74344353139e.js"
+  "/precache-manifest.c1ea4d62c1b37982cf27ddcaf9b5d8c0.js"
 );
 
 self.addEventListener('message', (event) => {
